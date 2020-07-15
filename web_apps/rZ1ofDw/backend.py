3267605ae0295a7652423cc0993022a5e166112c
admin <admin>
2020-07-15 12:44:27 -0400

    Saved web app dimreduction (DKU_TUTORIAL_ML_3.rZ1ofDw)

--- web_apps/rZ1ofDw/backend.py
+++ web_apps/rZ1ofDw/backend.py
@@ -22,7 +22,7 @@ df = df.set_index(df['customerID'], drop = True)
 df = df[['age_first_order','pages_visited_avg']]
 
 # Make plot 
-p = figure( plot_height=250)
+p = figure(plot_height=250)
 x = df.age_first_order
 y = df.pages_visited_avg
 p.scatter(x,y)
