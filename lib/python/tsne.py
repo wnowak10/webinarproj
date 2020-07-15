a1c9df00ae75fe64a8356e7b417c61a3ca9a8cd1
admin <admin>
2020-07-15 12:19:59 -0400

    edit file

---
+++ lib/python/tsne.py
@@ -0,0 +1,22 @@
+mydataset = dataiku.Dataset("customers_labeled")
+
+# Load the first lines.
+# You can also load random samples, limit yourself to some columns, or only load
+# data matching some filters.
+#
+# Please refer to the Dataiku Python API documentation for more information
+df = mydataset.get_dataframe(limit = dataset_limit)
+df = df.set_index(df['customerID'], drop = True)
+df_orig = df.copy()
+
+# Get the column names
+numerical_columns = list(df.select_dtypes(include=[np.number]).columns)
+categorical_columns = list(df.select_dtypes(include=[object]).columns)
+date_columns = list(df.select_dtypes(include=['<M8[ns]']).columns)
+
+# Print a quick summary of what we just loaded
+print "Loaded dataset"
+print "   Rows: %s" % df.shape[0]
+print "   Columns: %s (%s num, %s cat, %s date)" % (df.shape[1], 
+                                                    len(numerical_columns), len(categorical_columns),
+                                                    len(date_columns))
